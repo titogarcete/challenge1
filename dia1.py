734f6c79594d952783f228d30b904e3bfbe5e04f
titogarcete <144870935+titogarcete@users.noreply.github.com>
2024-06-24 18:58:47 -0400

    Add files via upload

---
+++ dia1.py
@@ -0,0 +1,8 @@
+# Solicitar al usuario que ingrese una cadena de caracteres
+cadena = input("Introduce una cadena de caracteres: ")
+
+# Invertir la cadena
+cadena_invertida = cadena[::-1]
+
+# Mostrar la cadena invertida
+print(f"La cadena invertida es: {cadena_invertida}")
